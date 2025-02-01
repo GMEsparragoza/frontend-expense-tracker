50322178b03eb3cddfb3166a90f7cd1253b5c284
GMEsparragoza <gianquizard0312@gmail.com>
2025-02-01 19:40:18 -0300

    Error de testing 429 v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useContext} from 'react';
+import React, { useEffect, useContext } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/DashboardPage';
 import { Navbar } from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './utils/AuthContext';
 import axios from 'axios'
 import Perfil from './pages/PerfilPage';
 import { AlertProvider } from './utils/AlertContext';
@@ -29,39 +28,37 @@ function App() {
   }, [resError, navigate]); // Se ejecuta cuando resError cambia
 
   return (
-    <AuthProvider>
-      <AlertProvider>
-        <BrowserRouter>
-          <Navbar />
-          <Alerta />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path='/reset-password' element={<ResetPassword />} />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <DateProvider>
-                    <Dashboard />
-                  </DateProvider>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/perfil"
-              element={
-                <ProtectedRoute>
-                  <Perfil />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/Error-Page-429" element={<ErrorFetch />} />
-          </Routes>
-        </BrowserRouter>
-      </AlertProvider>
-    </AuthProvider>
+    <AlertProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Alerta />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DateProvider>
+                  <Dashboard />
+                </DateProvider>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/perfil"
+            element={
+              <ProtectedRoute>
+                <Perfil />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/Error-Page-429" element={<ErrorFetch />} />
+        </Routes>
+      </BrowserRouter>
+    </AlertProvider>
   );
 }
 
